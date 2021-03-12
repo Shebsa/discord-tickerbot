574b09e55833ce32c41a9b65875183dee74d6ee4
Sebastian Jungmar <shebsa@gmail.com>
2021-03-12 20:32:05 +0100

    Added ability to add crypto tickers via Binance

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
 
+const Binance = require("node-binance-api");
+const binance = new Binance();
+
 const Discord = require("discord.js");
 
-const bots = [
+const stockBots = [
   {
     token: process.env.GME_BOT_TOKEN,
     ticker: process.env.GME_BOT_TICKER,
+  },
+  {
+    token: process.env.OMX_BOT_TOKEN,
+    ticker: process.env.OMX_BOT_TICKER,
   },
   {
     token: process.env.AMC_BOT_TOKEN,
@@ -14,32 +21,29 @@ const bots = [
   },
 ];
 
+const cryptoBots = [
+  {
+    token: process.env.BTC_BOT_TOKEN,
+    ticker: process.env.BTC_BOT_TICKER,
+    currency: "USD",
+  },
+];
+
 function relDiff(a, b) {
   return 100 * Math.abs((a - b) / ((a + b) / 2));
 }
 
-const fetchTickerData = async (client, ticker, index) => {
-  const url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance`;
+const setName = async (guild, index, currentPrice, currency) => {
+  guild.me.setNickname(`${index + 1}) ${currentPrice} ${currency}`);
+};
 
-  const response = await fetch(url);
-  const json = await response.json();
-
-  const percentage = relDiff(
-    json.chart.result[0].meta.regularMarketPrice,
-    json.chart.result[0].meta.previousClose
-  ).toFixed(2);
-
-  const diff = (
-    json.chart.result[0].meta.regularMarketPrice -
-    json.chart.result[0].meta.previousClose
-  ).toFixed(2);
-
+const setSubtitle = async (diff, percentage, client) => {
   const arrow = diff > 0 ? "↗" : "↘";
   const plusMinus = diff > 0 ? "+" : "-";
 
   const activityName = `${plusMinus}${Math.abs(
     diff
-  )} (${plusMinus}${percentage}%) ${arrow}`;
+  )} (${plusMinus}${percentage}%) ${arrow} | Bot created by @Shebsa#7397`;
   console.log(activityName);
 
   const activity = {
@@ -50,12 +54,9 @@ const fetchTickerData = async (client, ticker, index) => {
     status: "online",
   };
   client.user.setPresence(activity);
+};
 
-  const guild = await client.guilds.fetch(process.env.GUILD_ID);
-  guild.me.setNickname(
-    `${index + 1}) ${json.chart.result[0].meta.regularMarketPrice}`
-  );
-
+const setNameColor = async (guild, diff) => {
   const positiveRole = await guild.roles.fetch(process.env.POSITIVE_ROLE_ID);
   const negativeRole = await guild.roles.fetch(process.env.NEGATIVE_ROLE_ID);
 
@@ -74,7 +75,58 @@ const fetchTickerData = async (client, ticker, index) => {
   }
 };
 
-bots.forEach((bot, index) => {
+const fetchTickerData = async (client, ticker, index) => {
+  const url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance`;
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  const percentage = relDiff(
+    json.chart.result[0].meta.regularMarketPrice,
+    json.chart.result[0].meta.previousClose
+  ).toFixed(2);
+
+  const diff = (
+    json.chart.result[0].meta.regularMarketPrice -
+    json.chart.result[0].meta.previousClose
+  ).toFixed(2);
+
+  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+
+  setName(
+    guild,
+    index,
+    json.chart.result[0].meta.regularMarketPrice,
+    json.chart.result[0].meta.currency
+  );
+
+  setSubtitle(diff, percentage, client);
+
+  setNameColor(guild, diff);
+};
+
+const fetchCryptoTicker = async (client, bot, index) => {
+  binance.websockets.prevDay(bot.ticker, async (_, response) => {
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+
+    setName(
+      guild,
+      index,
+      parseFloat(response.averagePrice).toFixed(2),
+      bot.currency
+    );
+
+    setSubtitle(
+      parseFloat(response.priceChange).toFixed(2),
+      parseFloat(response.percentChange).toFixed(2),
+      client
+    );
+
+    setNameColor(guild, response.priceChange);
+  });
+};
+
+stockBots.forEach((bot, index) => {
   const client = new Discord.Client();
   client.login(bot.token);
 
@@ -84,5 +136,15 @@ bots.forEach((bot, index) => {
     setInterval(function () {
       fetchTickerData(client, bot.ticker, index);
     }, 5000);
+  });
+});
+
+cryptoBots.forEach((bot, index) => {
+  const client = new Discord.Client();
+  client.login(bot.token);
+
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    fetchCryptoTicker(client, bot, index);
   });
 });
