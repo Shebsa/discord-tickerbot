2dae05fe49ae0236fb55f667e570ab1f7b605cee
Sebastian Pettersson <shebsa@gmail.com>
2021-03-25 21:14:18 +0100

    Fixed percent diff error and added try catch

--- index.js
+++ index.js
@@ -8,34 +8,44 @@ const Discord = require("discord.js");
 
 const stockBots = [
   // {
+  //   token: process.env.OMX_BOT_TOKEN,
+  //   ticker: process.env.OMX_BOT_TICKER,
+  //   order: 1
+  // },
+  // {
+  //   token: process.env.SPY_BOT_TOKEN,
+  //   ticker: process.env.SPY_BOT_TICKER,
+  //   order: 2
+  // },
+  // {
   //   token: process.env.GME_BOT_TOKEN,
   //   ticker: process.env.GME_BOT_TICKER,
-  //   order: 1,
+  //   order: 1
   // },
   // {
   //   token: process.env.AMC_BOT_TOKEN,
   //   ticker: process.env.AMC_BOT_TICKER,
-  //   order: 2,
+  //   order: 2
   // },
 ];
 
 const cryptoBots = [
-  {
-    token: process.env.GME_BOT_TOKEN,
-    ticker: process.env.BTC_BOT_TICKER,
-    currency: "USD",
-    order: 1,
-  },
-  {
-    token: process.env.AMC_BOT_TOKEN,
-    ticker: process.env.ETH_BOT_TICKER,
-    currency: "USD",
-    order: 2,
-  },
+  // {
+  //   token: process.env.BTC_BOT_TOKEN,
+  //   ticker: process.env.BTC_BOT_TICKER,
+  //   currency: "USD",
+  //   order: 1
+  // },
+  // {
+  //   token: process.env.ETH_BOT_TOKEN,
+  //   ticker: process.env.ETH_BOT_TICKER,
+  //   currency: "USD",
+  //   order: 2
+  // },
 ];
 
 function relDiff(a, b) {
-  return 100 * Math.abs((a - b) / ((a + b) / 2));
+  return 100 * Math.abs((a - b) / a);
 }
 
 const setName = async (guild, index, currentPrice, currency) => {
@@ -48,7 +58,7 @@ const setSubtitle = async (diff, percentage, client) => {
 
   const activityName = `${plusMinus}${Math.abs(
     diff
-  )} (${plusMinus}${percentage}%) ${arrow} | Bot created by @Shebsa#7397`;
+  )} (${plusMinus}${percentage}%) ${arrow}`;
   console.log(activityName);
 
   const activity = {
@@ -81,63 +91,71 @@ const setNameColor = async (guild, diff) => {
 };
 
 const fetchTickerData = async (client, bot, index) => {
-  const url = `https://query1.finance.yahoo.com/v8/finance/chart/${bot.ticker}?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance`;
+  try {
+    const url = `https://query1.finance.yahoo.com/v8/finance/chart/${bot.ticker}?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance`;
 
-  const response = await fetch(url);
-  const json = await response.json();
+    const response = await fetch(url);
+    const json = await response.json();
 
-  const percentage = relDiff(
-    json.chart.result[0].meta.regularMarketPrice,
-    json.chart.result[0].meta.previousClose
-  ).toFixed(2);
+    const percentage = relDiff(
+      json.chart.result[0].meta.previousClose,
+      json.chart.result[0].meta.regularMarketPrice
+    ).toFixed(2);
 
-  const diff = (
-    json.chart.result[0].meta.regularMarketPrice -
-    json.chart.result[0].meta.previousClose
-  ).toFixed(2);
+    const diff = (
+      json.chart.result[0].meta.regularMarketPrice -
+      json.chart.result[0].meta.previousClose
+    ).toFixed(2);
 
-  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
 
-  setName(
-    guild,
-    bot.order,
-    json.chart.result[0].meta.regularMarketPrice,
-    json.chart.result[0].meta.currency
-  );
+    setName(
+      guild,
+      bot.order,
+      json.chart.result[0].meta.regularMarketPrice,
+      json.chart.result[0].meta.currency
+    );
 
-  setSubtitle(diff, percentage, client);
+    setSubtitle(diff, percentage, client);
 
-  setNameColor(guild, diff);
+    setNameColor(guild, diff);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const fetchCryptoTicker = async (client, bot) => {
-  const response = await coinGeckoClient.coins.fetch(bot.ticker, {
-    tickers: false,
-    market_data: true,
-    community_data: false,
-    developer_data: false,
-    localization: false,
-    sparkline: false,
-  });
+  try {
+    const response = await coinGeckoClient.coins.fetch(bot.ticker, {
+      tickers: false,
+      market_data: true,
+      community_data: false,
+      developer_data: false,
+      localization: false,
+      sparkline: false,
+    });
 
-  console.info(response.data.market_data.current_price.usd);
-  const guild = await client.guilds.fetch(process.env.GUILD_ID);
-  setName(
-    guild,
-    bot.order,
-    response.data.market_data.current_price.usd,
-    bot.currency
-  );
-  setSubtitle(
-    response.data.market_data.price_change_24h_in_currency.usd,
-    response.data.market_data.price_change_percentage_24h.toFixed(2),
-    client
-  );
+    console.info(response.data.market_data.current_price.usd);
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    setName(
+      guild,
+      bot.order,
+      response.data.market_data.current_price.usd,
+      bot.currency
+    );
+    setSubtitle(
+      response.data.market_data.price_change_24h_in_currency.usd,
+      response.data.market_data.price_change_percentage_24h.toFixed(2),
+      client
+    );
 
-  setNameColor(
-    guild,
-    response.data.market_data.price_change_24h_in_currency.usd
-  );
+    setNameColor(
+      guild,
+      response.data.market_data.price_change_24h_in_currency.usd
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 stockBots.forEach((bot, index) => {
